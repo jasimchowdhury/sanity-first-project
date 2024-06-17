d345fed5a94adfe37dd05603c9e4ef93269509fc
Jasim <abir_jasim@yahoo.com>
2024-06-17 21:22:03 +0100

    problem solved

--- me-and-pic/sanity/schemas/sanity-utilis.ts
+++ me-and-pic/sanity/schemas/sanity-utilis.ts
@@ -1,6 +1,7 @@
 import { createClient, groq } from "next-sanity";
+import { Project } from "../../types/Project";
 
-export async function getProject(){
+export async function getProject(): Promise<Project[]>{
     const client = createClient({
         projectId: '54x35z0z',
         dataset: 'production',
@@ -8,7 +9,7 @@ export async function getProject(){
     });
 
     return client.fetch(
-        groq`*[_type = 'project']{
+        groq`*[_type == 'project']{
         _id,
         _createdAt,
         name,
--- me-and-pic/src/app/page.tsx
+++ me-and-pic/src/app/page.tsx
@@ -1,11 +1,22 @@
+import Image from "next/image";
 import { getProject } from "../../sanity/schemas/sanity-utilis";
 
+// export default async function Home() {
+//   const projects = await getProject();
+//   return (
+// <div>
+//   {projects.map((project) => (
+//     <div key={project._id}>{project.name}</div>
+//   ))}
+// </div>
+//   );
+// }
 export default async function Home() {
   const projects = await getProject();
   return (
     <div>
       {projects.map((project) => (
-        <div key={project.id}>{project.name}</div>
+        <div key={project._id}>{project.name}</div>
       ))}
     </div>
   );
